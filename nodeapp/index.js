c28b084152f0ff79ae290a1ce91a56f512128574
ShaikhSharukh12 <86522478+ShaikhSharukh12@users.noreply.github.com>
2022-06-27 01:18:48 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello, Welcome to bangalore" }');
+    res.send('{ "response": "Hello, Welcome to Bangalore.." }');
 });
 
 app.get('/will', function (req, res) {
